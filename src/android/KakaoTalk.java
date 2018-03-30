8c6baa2661fb7ab973a2ba432d0462bbf60aad7a
dylee <leeduyoung2002@gmail.com>
2018-03-30 17:02:35 +0900

    sytax error fix

--- src/android/KakaoTalk.java
+++ src/android/KakaoTalk.java
@@ -309,7 +309,7 @@ public class KakaoTalk extends CordovaPlugin {
                 public void onNotSignedUp() {
                     callbackContext.error("this user is not signed up");
                 }
-            }, propertyKeys, false););
+            }, propertyKeys, false));
         }
 
         @Override
