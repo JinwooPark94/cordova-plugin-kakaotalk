bbf3fbc0f638b6024c91b38486de3514516b1508
dylee <leeduyoung2002@gmail.com>
2018-03-30 16:07:59 +0900

    로그인 성공시 kakao accesstoken return

--- src/ios/KakaoTalk.m
+++ src/ios/KakaoTalk.m
@@ -12,24 +12,26 @@
     if ([[KOSession sharedSession] isOpen]) {
       // 로그인 성공
       // 사용자 정보 요청
-      [KOSessionTask meTaskWithCompletionHandler:^(KOUser* result, NSError *error) {
-        CDVPluginResult* pluginResult = nil;
-        if (result) {
+    //   [KOSessionTask meTaskWithCompletionHandler:^(KOUser* result, NSError *error) {
+    //     CDVPluginResult* pluginResult = nil;
+    //     if (result) {
 
-          // 리턴
-          NSDictionary *userSession = @{
-            @"id": result.ID,
-            @"nickname": [result propertyForKey:KOUserNicknamePropertyKey],
-            @"email": result.email,
-            @"profileImage":[result propertyForKey:KOUserProfileImagePropertyKey]
-          };
-          pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:userSession];
-        } else {
-          // 사용자 정보 요청 실패
-          pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]];
-        }
+    //       // 리턴
+    //       NSDictionary *userSession = @{
+    //         @"id": result.ID,
+    //         @"nickname": [result propertyForKey:KOUserNicknamePropertyKey],
+    //         @"email": result.email,
+    //         @"profileImage":[result propertyForKey:KOUserProfileImagePropertyKey]
+    //       };
+    //       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:userSession];
+    //     } else {
+    //       // 사용자 정보 요청 실패
+    //       pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]];
+    //     }
+    //     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    //   }];
+        CDVPluginResult* pluginResult = pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[KOSession sharedSession].accessToken];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-      }];
     } else {
       // 로그인 실패
       CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]];
